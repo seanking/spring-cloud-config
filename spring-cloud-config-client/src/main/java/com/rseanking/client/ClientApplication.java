73467adb619563ddd72cc89b99f2e0ea831dbfbf
Sean King <r.sean.king@gmail.com>
2017-12-31 12:35:15 -0500

    Simplify application annotation to be SpringBootApplication

--- spring-cloud-config-client/src/main/java/com/rseanking/client/ClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/rseanking/client/ClientApplication.java
@@ -2,12 +2,12 @@ package com.rseanking.client;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @RestController
 public class ClientApplication {
 	@Value("${hello.greeting:Hello}")
